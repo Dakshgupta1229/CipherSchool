2b7b7f93340643aaa5b57a82f8268daaa9144b2f
Daksh Gupta <145596305+Dakshgupta1229@users.noreply.github.com>
2024-08-26 17:15:29 +0530

    Add files via upload

---
+++ Day-04.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // Ques:01: Write a C++ program that takes two integers as input and performs the following arithmetic operations:
+
+    // int num1,num2;
+    // cout<<"Enter first integer: ";
+    // cin>>num1;
+    // cout<<"Enter second integer: ";
+    // cin>>num2;
+    // cout<<"Addition: "<<num1+num2<<endl;
+    // cout<<"Subtraction: "<<num1-num2<<endl;
+    // cout<<"Multiplication: "<<num1*num2<<endl;
+    // if(num2==0){
+    //     cout<<"Invalid"<<endl;
+    // }
+    // else{
+    //     cout<<"Division: "<<(float)num1/num2<<endl;
+    // }
+    // cout<<"Modulus: "<<num1%num2<<endl;
+
+
+    return 0;
+}
